a6dbf4c9748bc5b8862b415afa039ea30093cd50
Saksham Mathur <saksham.mathur25@gmail.com>
2022-08-07 22:25:53 +0530

    Added signup api for admin and user and support for cloudinary sdk

--- controllers/admin/admin.js
+++ controllers/admin/admin.js
@@ -2,145 +2,187 @@ const bcrypt = require('bcrypt');
 const User = require('../../models/Users');
 const Book = require('../../models/Books');
 const Category = require('../../models/Category');
+const cloudinary = require('cloudinary').v2;
+
+const signUp = (req, res) => {
+    const {fullName, email, password, confirmPassword, isAdmin} = req.body;
+
+    try {
+        if (!fullName || !email || !password || !confirmPassword) {
+            return res.status(400).json({
+                message: 'Please fill all the fields',
+            });
+        }
+        if (password !== confirmPassword) {
+            return res.status(400).json({
+                message: 'Passwords do not match',
+            });
+        }
+
+        const user = new User({
+            fullName,
+            email,
+            password,
+            isAdmin,
+        });
+        user.save().then(() => {
+            if (isAdmin) {
+                res.status(200).json({
+                    message: 'Admin created successfully',
+                });
+            } else {
+                res.status(200).json({
+                    message: 'User created successfully',
+                });
+            }
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
+}
 
 const login = (req, res) => {
-	const { email, password } = req.body;
+    const {email, password} = req.body;
 
-	try {
-		if (!email || !password) {
-			return res.status(400).json({
-				message: 'Please fill all the fields',
-			});
-		}
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: 'Please fill all the fields',
+            });
+        }
 
-		User.findOne({ email }).then((user) => {
-			if (!user) {
-				return res.status(400).json({
-					message: 'User does not exist',
-				});
-			}
+        User.findOne({email}).then((user) => {
+            if (!user) {
+                return res.status(400).json({
+                    message: 'User does not exist',
+                });
+            }
 
-			const isMatch = bcrypt.compareSync(password, user?.password);
+            const isMatch = bcrypt.compareSync(password, user?.password);
 
-			if (!isMatch) {
-				return res.status(400).json({
-					message: 'Invalid password',
-				});
-			}
+            if (!isMatch) {
+                return res.status(400).json({
+                    message: 'Invalid password',
+                });
+            }
 
-			res.status(200).json({
-				message: 'User logged in successfully',
-				user: {
-					id: user._id,
-					name: user.name,
-					email: user.email,
-					image: user.image,
-					isAdmin: user.isAdmin,
-				},
-			});
-		});
-	} catch (e) {
-		res.status(400).json({
-			message: e.message,
-		});
-	}
+            res.status(200).json({
+                message: 'User logged in successfully',
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    image: user.image,
+                    isAdmin: user.isAdmin,
+                },
+            });
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
 };
 
 const createBook = async (req, res) => {
-	const { title, author, description, image, category, createdBy } = req.body;
+    const {title, author, description, category, createdBy} = req.body;
 
-	try {
-		if (!title || !author || !description || !image) {
-			return res.status(400).json({
-				message: 'Please fill all the fields',
-			});
-		}
+    const {image} = req.file;
 
-		const book = new Book({
-			title,
-			author,
-			description,
-			image,
-			category,
-			createdBy,
-		});
+    try {
+        if (!title || !author || !description || !image) {
+            return res.status(400).json({
+                message: 'Please fill all the fields',
+            });
+        }
 
-		await book
-			.save()
-			.then((book) => {
-				res.status(201).json({
-					message: 'Book created successfully',
-					book,
-				});
-			})
-			.catch((e) => {
-				res.status(400).json({
-					message: e.message,
-				});
-			});
-	} catch (e) {
-		res.status(400).json({
-			message: e.message,
-		});
-	}
+        const book = new Book({
+            title,
+            author,
+            description,
+            image,
+            category,
+            createdBy,
+        });
+
+        await cloudinary.uploader.upload(image, async (err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    message: err.message,
+                });
+            }
+            book.image = result.url;
+            await book.save();
+            res.status(200).json({
+                message: 'Book created successfully',
+                book,
+            });
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
 };
 
 const deleteBook = async (req, res) => {
-	const { id } = req.params;
+    const {id} = req.params;
 
-	try {
-		await Book.findByIdAndDelete(id);
+    try {
+        await Book.findByIdAndDelete(id);
 
-		res.status(200).json({
-			message: 'Book deleted successfully',
-		});
-	} catch (e) {
-		res.status(400).json({
-			message: e.message,
-		});
-	}
+        res.status(200).json({
+            message: 'Book deleted successfully',
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
 };
 
 const addCategory = async (req, res) => {
-	const { name } = req.body;
+    const {name} = req.body;
 
-	try {
-		if (!name) {
-			return res.status(400).json({
-				message: 'Please fill all the fields',
-			});
-		}
+    try {
+        if (!name) {
+            return res.status(400).json({
+                message: 'Please fill all the fields',
+            });
+        }
 
-		const category = new Category({
-			name,
-		});
+        const category = new Category({
+            name,
+        });
 
-		await category.save();
+        await category.save();
 
-		res.status(200).json({
-			message: 'Category created successfully',
-		});
-	} catch (e) {
-		res.status(400).json({
-			message: e.message,
-		});
-	}
+        res.status(200).json({
+            message: 'Category created successfully',
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
 };
 
 const listAllBooks = async (req, res) => {
-	const query = req.query.createdBy;
-	try {
-		const books = await Book.find({ createdBy: query });
+    const query = req.query.createdBy;
+    try {
+        const books = await Book.find({createdBy: query});
 
-		res.status(200).json({
-			message: 'Books fetched successfully',
-			books,
-		});
-	} catch (e) {
-		res.status(400).json({
-			message: e.message,
-		});
-	}
+        res.status(200).json({
+            message: 'Books fetched successfully',
+            books,
+        });
+    } catch (e) {
+        res.status(400).json({
+            message: e.message,
+        });
+    }
 };
 
-module.exports = { login, createBook, deleteBook, listAllBooks, addCategory };
+module.exports = {login, createBook, deleteBook, listAllBooks, addCategory,signUp};
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,6 +2,7 @@ const {signUp,login,createBook,listAllBooks,deleteBook,addCategory} = require(".
 const router = require('express').Router();
 
 router.post('/login', login);
+router.post('/signup', signUp);
 router.post('/create-book/', createBook);
 router.get('/get-books/',listAllBooks);
 router.delete('/delete-book/:id',deleteBook);
